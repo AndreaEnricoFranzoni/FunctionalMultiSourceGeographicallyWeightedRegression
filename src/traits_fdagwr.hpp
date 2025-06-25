8af5f9af5e725af7c62bdee19f45dc25463575a9
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-06-25 12:01:31 +0200

    typo

--- src/traits_fdagwr.hpp
+++ src/traits_fdagwr.hpp
@@ -65,7 +65,7 @@ public:
   
   using Dense_Array   = Eigen::ArrayXd;                  ///< Array data structure: more efficient for coefficient-wise operations.
 
-  using fdagwr_domain = fdapde::Triangulation<1, 1>      ///< Domain mesh: unit interval with a fixed number of nodes 
+  using fdagwr_domain = fdapde::Triangulation<1, 1>;     ///< Domain mesh: unit interval with a fixed number of nodes 
 
 };
 
