7e9009418be6da2cfc41efd2a49406d4830bea59
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-06-26 11:39:56 +0200

    trying to construct Bs Objects

--- src/basis_systems.hpp
+++ src/basis_systems.hpp
@@ -65,7 +65,7 @@ public:
                         std::cout << "Intervallo generato, con i seguenti nodi:" << std::endl;
                         std::cout << interval.nodes() << std::endl;
 
-                        //fdapde::BsSpace<domain_structure> Vh(interval, 3);
+                        fdapde::BsSpace<domain_structure> Vh(interval, 3);
 
 
                         //construct the basis system
