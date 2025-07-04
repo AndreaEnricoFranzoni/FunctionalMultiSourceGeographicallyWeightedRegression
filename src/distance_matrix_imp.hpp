76c30a7304e926ca7dbd1cdf0f5914fb85e3d718
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-07-04 17:41:01 +0200

    typo

--- src/distance_matrix_imp.hpp
+++ src/distance_matrix_imp.hpp
@@ -59,7 +59,7 @@ distance_matrix<distance_measure>::compute_distances()
     m_distances.resize(m_number_dist_comp);
 
     for(std::size_t j = 0; j < m_number_locations; ++j){
-        for (std::size_t i = 0; i < j; ++i)
+        for (std::size_t i = 0; i <= j; ++i)
         {
             
             std::size_t k = (i*(i+static_cast<std::size_t>(1)))/static_cast<std::size_t>(2) + j;
