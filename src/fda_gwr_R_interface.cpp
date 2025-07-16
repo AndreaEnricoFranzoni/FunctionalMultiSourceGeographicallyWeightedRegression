c642489fb2fdf04381d498a4a365dd6941cece4e
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-07-16 16:28:25 +0200

    typo

--- src/fda_gwr_R_interface.cpp
+++ src/fda_gwr_R_interface.cpp
@@ -282,7 +282,7 @@ Rcpp::List fmsgwr(Rcpp::NumericMatrix y_points,
 
 
     //COMPUTING THE BASIS
-    basis_systems< BASIS_TYPE::BSPLINES > bs(knots_stationary_cov_eigen_w_, order_basis_stationary_cov_, number_basis_stationary_cov_, q_C);
+    basis_systems< fdagwr_traits::Domain, BASIS_TYPE::BSPLINES > bs(knots_stationary_cov_eigen_w_, order_basis_stationary_cov_, number_basis_stationary_cov_, q_C);
 /*
     for(std::size_t i=0; i < bs.q(); ++i)
     {
