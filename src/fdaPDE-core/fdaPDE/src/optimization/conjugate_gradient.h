1916c7604b5b265f30b5113ed4a24f9746b1724f
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-07-21 10:39:24 +0200

    updating fdaPDE version accepting order 0 bsplines

--- src/fdaPDE-core/fdaPDE/src/optimization/conjugate_gradient.h
+++ src/fdaPDE-core/fdaPDE/src/optimization/conjugate_gradient.h
@@ -14,15 +14,17 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef __FDAPDE_GRADIENT_DESCENT_H__
-#define __FDAPDE_GRADIENT_DESCENT_H__
+#ifndef __FDAPDE_CONJUGATE_GRADIENT_H__
+#define __FDAPDE_CONJUGATE_GRADIENT_H__
 
 #include "header_check.h"
 
 namespace fdapde {
 
-// implementation of Gradient Descent algorithm for unconstrained nonlinear optimization
-template <int N, typename... Args> class GradientDescent {
+namespace internals {
+
+// implementation of Conjugate Gradient method for unconstrained nonlinear optimization
+template <int N, typename DirectionMethod, typename... Args> class conjugate_gradient_impl {
    private:
     using vector_t = std::conditional_t<N == Dynamic, Eigen::Matrix<double, Dynamic, 1>, Eigen::Matrix<double, N, 1>>;
     using matrix_t =
@@ -37,19 +39,18 @@ template <int N, typename... Args> class GradientDescent {
     int n_iter_ = 0;   // current iteration number
    public:
     vector_t x_old, x_new, update, grad_old, grad_new;
-    matrix_t inv_hessian;
     double h;
     // constructor
-    GradientDescent() = default;
-    GradientDescent(int max_iter, double tol, double step)
+    conjugate_gradient_impl() = default;
+    conjugate_gradient_impl(int max_iter, double tol, double step)
         requires(sizeof...(Args) != 0)
         : max_iter_(max_iter), tol_(tol), step_(step) { }
-    GradientDescent(int max_iter, double tol, double step, Args&&... callbacks) :
+    conjugate_gradient_impl(int max_iter, double tol, double step, Args&&... callbacks) :
         callbacks_(std::make_tuple(std::forward<Args>(callbacks)...)), max_iter_(max_iter), tol_(tol), step_(step) { }
     // copy semantic
-    GradientDescent(const GradientDescent& other) :
+    conjugate_gradient_impl(const conjugate_gradient_impl& other) :
         callbacks_(other.callbacks_), max_iter_(other.max_iter_), tol_(other.tol_), step_(other.step_) { }
-    GradientDescent& operator=(const GradientDescent& other) {
+    conjugate_gradient_impl& operator=(const conjugate_gradient_impl& other) {
         max_iter_ = other.max_iter_;
         tol_ = other.tol_;
         step_ = other.step_;
@@ -64,20 +65,22 @@ template <int N, typename... Args> class GradientDescent {
           INVALID_CALL_TO_OPTIMIZE__OBJECTIVE_FUNCTOR_NOT_ACCEPTING_VECTORTYPE);
         bool stop = false;   // asserted true in case of forced stop
         double error = std::numeric_limits<double>::max();
-        h = step_;
+	DirectionMethod beta;
+        auto grad = objective.gradient();
         n_iter_ = 0;
+        h = step_;
         x_old = x0, x_new = x0;
-	auto grad = objective.gradient();
         grad_old = grad(x_old);
+        update = -grad_old;
 
         while (n_iter_ < max_iter_ && error > tol_ && !stop) {
-            update = -grad_old;
             stop |= execute_pre_update_step(*this, objective, callbacks_);
             // update along descent direction
             x_new = x_old + h * update;
             grad_new = grad(x_new);
             if constexpr (sizeof...(Functor) == 1) { (func(objective(x_old)), ...); }
-            // prepare next iteration
+	    // prepare next iteration
+            update = -grad_new + std::max(0.0, beta(*this)) * update;   // update conjugate direction
             error = grad_new.norm();
             stop |=
               (execute_post_update_step(*this, objective, callbacks_) || execute_stopping_criterion(*this, objective));
@@ -87,7 +90,7 @@ template <int N, typename... Args> class GradientDescent {
         }
         optimum_ = x_old;
         value_ = objective(optimum_);
-	if constexpr (sizeof...(Functor) == 1) { (func(value_), ...); }
+        if constexpr (sizeof...(Functor) == 1) { (func(value_), ...); }
         return optimum_;
     }
     // getters
@@ -96,6 +99,44 @@ template <int N, typename... Args> class GradientDescent {
     int n_iter() const { return n_iter_; }
 };
 
+struct fletcher_reeves_impl {
+    template <typename Opt> double operator()(const Opt& opt) { return opt.grad_new.norm() / opt.grad_old.norm(); }
+};
+struct polak_ribiere_impl {
+    template <typename Opt> double operator()(const Opt& opt) {
+        return opt.grad_new.dot(opt.grad_new - opt.grad_old) / opt.grad_old.norm();
+    }
+};
+
+}   // namespace internals
+
+// public CG optimizers
+template <int N, typename... Args>
+class FletcherReevesCG : public internals::conjugate_gradient_impl<N, internals::fletcher_reeves_impl, Args...> {
+   private:
+    using Base = internals::conjugate_gradient_impl<N, internals::fletcher_reeves_impl, Args...>;
+   public:
+    FletcherReevesCG() = default;
+    FletcherReevesCG(int max_iter, double tol, double step)
+        requires(sizeof...(Args) != 0)
+        : Base(max_iter, tol, step) { }
+    FletcherReevesCG(int max_iter, double tol, double step, Args&&... callbacks) :
+        Base(max_iter, tol, step, callbacks...) { }
+};
+
+template <int N, typename... Args>
+class PolakRibiereCG : public internals::conjugate_gradient_impl<N, internals::polak_ribiere_impl, Args...> {
+   private:
+    using Base = internals::conjugate_gradient_impl<N, internals::polak_ribiere_impl, Args...>;
+   public:
+    PolakRibiereCG() = default;
+    PolakRibiereCG(int max_iter, double tol, double step)
+        requires(sizeof...(Args) != 0)
+        : Base(max_iter, tol, step) { }
+    PolakRibiereCG(int max_iter, double tol, double step, Args&&... callbacks) :
+        Base(max_iter, tol, step, callbacks...) { }
+};
+
 }   // namespace fdapde
 
-#endif   // __FDAPDE_GRADIENT_DESCENT_H__
+#endif   // __FDAPDE_CONJUGATE_GRADIENT_H__
