c373d2b4f0453cd27b3aaf0ce02a03b7ef87ecef
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-01 18:22:46 +0200

    typo

--- src/fda_gwr_R_interface.cpp
+++ src/fda_gwr_R_interface.cpp
@@ -194,7 +194,7 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     //SOLO PER LE COORDINATE OGNI RIGA E' UN'UNITA'
 
 
-    Rcout << "fdagwr.19: " << std::endl;
+    Rcout << "fdagwr.39: " << std::endl;
 
     using _DATA_TYPE_ = double;                                                     //data type
     using _FD_INPUT_TYPE_ = FDAGWR_TRAITS::fd_obj_x_type;                           //data type for the abscissa of fdata (double)
