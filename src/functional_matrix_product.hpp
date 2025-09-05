a0abb7000f4ee3011282cfd48747295d947ff832
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-05 12:15:50 +0200

    code cleaned in matrix matrix product

--- src/functional_matrix_product.hpp
+++ src/functional_matrix_product.hpp
@@ -50,9 +50,7 @@ fm_product(const functional_matrix<INPUT,OUTPUT> &M1,
     {
         for (std::size_t j = 0; j < cols_prod; ++j)
         {
-            auto col_j = M2.get_col(j);
-            col_j.transposing();
-            functional_matrix<INPUT,OUTPUT> prod_ij = M1.get_row(i)*col_j;
+            functional_matrix<INPUT,OUTPUT> prod_ij = M1.get_row(i)*(M2.get_col(j).transpose());
             prod(i,j) = prod_ij.reduce();
         }
     }
