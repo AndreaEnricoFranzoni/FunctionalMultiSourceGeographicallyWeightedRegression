4245f3c62aeae5e4a37028a5a20db57723fcdc29
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-16 14:23:53 +0200

    rewriting the dense*sparse

--- src/functional_matrix_product.hpp
+++ src/functional_matrix_product.hpp
@@ -119,10 +119,16 @@ fm_prod(const functional_matrix<INPUT,OUTPUT> &M1,
         std::size_t start_col_j = SM2.cols_idx()[j];
         std::size_t end_col_j = SM2.cols_idx()[j+1];
         
-        for(std::size_t i = 0; i < prod.rows(); ++i){
-            //for each element, making the product looping only of the non null elements
-            for(auto non_null_row = std::next(SM2.rows_idx().cbegin(),start_col_j); non_null_row != std::next(SM2.rows_idx().cbegin(),end_col_j); ++non_null_row){
-                prod(i,j) = f_sum(prod(i,j),f_prod(M1(i,*non_null_row),SM2(*non_null_row,j)));}}}
+        if(start_col_j <  end_col_j){
+            for(std::size_t i = 0; i < prod.rows(); ++i){
+                //for each element, making the product looping only of the non null elements
+                //for(auto non_null_row = std::next(SM2.rows_idx().cbegin(),start_col_j); non_null_row != std::next(SM2.rows_idx().cbegin(),end_col_j); ++non_null_row){
+                //    prod(i,j) = f_sum(prod(i,j),f_prod(M1(i,*non_null_row),SM2(*non_null_row,j)));}
+                std::for_each(std::next(SM2.rows_idx().cbegin(),start_col_j),
+                              std::next(SM2.rows_idx().cbegin(),end_col_j),
+                              [i,j,&f_sum,&f_prod](const std::size_t &row_no_null){ prod(i,j) = f_sum(prod(i,j),f_prod(M1(i,row_no_null),SM2(row_no_null,j)));});
+                }}
+    }
 
     return prod;
 }
