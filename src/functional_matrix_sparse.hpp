8a5d5438c5e38bb3a681e7758678f5f5f63d6491
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-17 18:32:04 +0200

    print for debugging

--- src/functional_matrix_sparse.hpp
+++ src/functional_matrix_sparse.hpp
@@ -338,6 +338,7 @@ public:
                 if(m_cols_idx[i] > m_cols_idx[i-1]){
                     m_rows_idx.emplace_back(i-1);}}
             //m_cols_idx: only one column containing all the elements
+            m_cols_idx.clear();
             std::vector<std::size_t> new_cols_idx{0,m_nnz};
             m_cols_idx = new_cols_idx;
             new_cols_idx.clear();
