343748c003be11f5b26de2baf128e6cab1e3fbd4
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-17 18:23:44 +0200

    print for debugging

--- src/fda_gwr_R_interface.cpp
+++ src/fda_gwr_R_interface.cpp
@@ -602,6 +602,7 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     //Rcout << "Trasposto di una completa" << std::endl;
     auto test_sm_t = test_sm.transpose();
 
+/*
     Rcout << "Matrice generale" << std::endl;
     for(std::size_t i = 0; i < test_sm.rows(); ++i){
         for(std::size_t j = 0; j < test_sm.cols(); ++j){
@@ -621,6 +622,7 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     for(std::size_t i = 0; i < test_sm_t.rows_idx().size(); ++i){Rcout << test_sm_t.rows_idx()[i] << std::endl;}
     Rcout << "SM T cols idx" << std::endl;
     for(std::size_t i = 0; i < test_sm_t.size(); ++i){Rcout << test_sm_t.cols_idx()[i] << std::endl;}
+*/
 
 
 
@@ -632,8 +634,6 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     //Rcout << "Trasposto di un vettore colonna" << std::endl;
     auto test_sm_cv_t = test_sm_cv.transpose();
 
-
-/*
     Rcout << "Vettore colonna" << std::endl;
     for(std::size_t i = 0; i < test_sm_cv.rows(); ++i){
         for(std::size_t j = 0; j < test_sm_cv.cols(); ++j){
@@ -653,7 +653,7 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     for(std::size_t i = 0; i < test_sm_cv_t.rows_idx().size(); ++i){Rcout << test_sm_cv_t.rows_idx()[i] << std::endl;}
     Rcout << "CV T cols idx" << std::endl;
     for(std::size_t i = 0; i < test_sm_cv_t.size(); ++i){Rcout << test_sm_cv_t.cols_idx()[i] << std::endl;}
-*/
+
 
 
 
