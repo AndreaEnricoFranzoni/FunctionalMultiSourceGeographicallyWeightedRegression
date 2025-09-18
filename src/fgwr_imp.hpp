a752767c0022196f711329536d42b27de3227751
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-18 15:43:28 +0200

    using .eval() to materialize the ETs

--- src/fgwr_imp.hpp
+++ src/fgwr_imp.hpp
@@ -46,7 +46,7 @@ const
         functional_matrix<INPUT,OUTPUT> integrand = fm_prod(fm_prod(fm_prod(fm_prod(base_t,X_t),W[i],m_number_threads),X,m_number_threads),base);
 
         //performing integration and factorization 
-        FDAGWR_TRAITS::Dense_Matrix inverse_penalty = this->fm_integration(integrand) + _R_;
+        FDAGWR_TRAITS::Dense_Matrix inverse_penalty = (this->fm_integration(integrand) + _R_).eval();
         penalty[i] = Eigen::PartialPivLU<FDAGWR_TRAITS::Dense_Matrix>(inverse_penalty);    //.eval() is needed to evaluate the lazy expression of ETs  
         // penalty[i].solve(M) equivale a fare elemento penalty[i], che è una matrice inversa, times M
     }
@@ -54,3 +54,7 @@ const
     
     return penalty;
 }
+
+
+
+
