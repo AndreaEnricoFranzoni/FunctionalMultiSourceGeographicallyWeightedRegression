c8e664442d66288c10a654b1ca40ac7bad25d392
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-26 16:43:59 +0200

    print for debugging

--- src/fda_gwr_R_interface.cpp
+++ src/fda_gwr_R_interface.cpp
@@ -543,7 +543,7 @@ Rcpp::List FMSGWR(Rcpp::NumericMatrix y_points,
     functional_matrix_sparse<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> psi = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,bsplines_basis>(bs_S);
 
 
-    Rcout << "fdagwr.04:" << std::endl;
+    Rcout << "fdagwr.05:" << std::endl;
     //fgwr algorithm
     auto fgwr_algo = fgwr_factory< _FGWR_ALGO_, _FD_INPUT_TYPE_, _FD_OUTPUT_TYPE_ >(std::move(y),
                                                                                     std::move(phi),
