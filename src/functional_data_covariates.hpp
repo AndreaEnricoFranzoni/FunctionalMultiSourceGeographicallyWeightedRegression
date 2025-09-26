b11aec9da31ffa48748f2ec140a63fe7c7b8e90a
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-09-26 16:06:53 +0200

    using push_back in two points of the code where gcc14 complains

--- src/functional_data_covariates.hpp
+++ src/functional_data_covariates.hpp
@@ -57,7 +57,9 @@ public:
                             for(std::size_t i = 0; i < m_q; ++i)
                             {
                                 std::unique_ptr<basis_base_class<domain_type>> basis_i = factoryBasis.create(basis_types[i],knots,basis_degrees[i],basis_numbers[i]);
-                                m_X.emplace_back(std::move(coeff[i]),std::move(basis_i));
+                                //m_X.emplace_back(std::move(coeff[i]),std::move(basis_i));
+                                functional_data< domain_type,basis_base_class > x_i(std::move(coeff[i]),std::move(basis_i));
+                                m_X.push_back(x_i);
                             }
                         }
     
