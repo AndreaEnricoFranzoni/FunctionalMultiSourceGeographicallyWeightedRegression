890e7328987f8d35bc8ee5d86a3b67bdd0459780
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-10-06 17:41:31 +0200

    typo

--- src/utility/utility_fdagwr.hpp
+++ src/utility/utility_fdagwr.hpp
@@ -439,7 +439,6 @@ wrap_PRes_to_R_list(const PartialResidualTuple& p_res)
 
     //two sources
     if constexpr (std::is_same_v<T, std::tuple< FDAGWR_TRAITS::Dense_Matrix, std::vector< FDAGWR_TRAITS::Dense_Matrix >, std::vector< FDAGWR_TRAITS::Dense_Matrix >>>) {
-      auto [a, b, c] = val;
       return Rcpp::List::create(Rcpp::Named("c_tilde_hat") = Rcpp::wrap(std::get<0>(tup)),
                                 Rcpp::Named("A__")         = toRList(std::get<1>(tup),false),
                                 Rcpp::Named("B__for_K")    = toRList(std::get<2>(tup),false));
