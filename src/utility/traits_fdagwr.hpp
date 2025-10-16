e1b0842adb909ff8375e08d49f2f38ba62380d43
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-10-16 15:10:18 +0200

    function for Functional Mixed Geographically Weighted Regression

--- src/utility/traits_fdagwr.hpp
+++ src/utility/traits_fdagwr.hpp
@@ -148,7 +148,7 @@ struct FDAGWR_BETAS_NAMES
 
 using PartialResidualTuple = std::variant<
     std::monostate,
-    std::tuple< int, int >,
+    std::tuple< FDAGWR_TRAITS::Dense_Matrix, std::vector< FDAGWR_TRAITS::Dense_Matrix > >,
     std::tuple< FDAGWR_TRAITS::Dense_Matrix, std::vector< FDAGWR_TRAITS::Dense_Matrix >, std::vector< FDAGWR_TRAITS::Dense_Matrix > >
 >;
 
