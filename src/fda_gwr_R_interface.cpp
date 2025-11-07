b8011d795c50391d7e396214216823b706b8b8e7
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2025-11-07 16:19:52 +0100

    typo

--- src/fda_gwr_R_interface.cpp
+++ src/fda_gwr_R_interface.cpp
@@ -7593,11 +7593,11 @@ Rcout << "Covariates to be pred" << std::endl;
                                                                       knots_stations_cov_eigen_w_,
                                                                       basis_fac);
     //Xc_new: a functional matrix of dimension n_newxqc
-    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xc_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_STATIONARY_>(x_C_fd_to_be_pred_,num_threads);                                                               
+    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xc_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_STATIONARY_>(x_C_fd_to_be_pred_,number_threads);                                                               
     //Xe_new: a functional matrix of dimension n_newxqe
-    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xe_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_EVENT_>(x_E_fd_to_be_pred_,num_threads);
+    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xe_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_EVENT_>(x_E_fd_to_be_pred_,number_threads);
     //Xs_new: a functional matrix of dimension n_newxqs
-    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xs_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_STATION_>(x_S_fd_to_be_pred_,num_threads);
+    functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_> Xs_new = wrap_into_fm<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_,_DOMAIN_,_STATION_>(x_S_fd_to_be_pred_,number_threads);
     //map containing the X
     std::map<std::string,functional_matrix<_FD_INPUT_TYPE_,_FD_OUTPUT_TYPE_>> X_new = {
         {std::string{covariate_type<_STATIONARY_>()},Xc_new},
@@ -7628,7 +7628,7 @@ std::cout << "Quasi nel New constructor" << std::endl;
                                                                                                  b,
                                                                                                  1,
                                                                                                  n_train,
-                                                                                                 num_threads,
+                                                                                                 number_threads,
                                                                                               in_cascade_estimation);
 
 /* 
